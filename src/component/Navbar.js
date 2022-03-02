512aef4e24f8a670952be3239db4023c6bbde07f
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-02 20:21:44 +0100

    Create nav links

---
+++ src/component/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: '00 HOME'
+    },
+    {
+      id: 2,
+      path: '/destination',
+      text: '01 DESTINATION'
+    },
+    {
+      id: 3,
+      path: '/crew',
+      text: '02 CREW'
+    },
+    {
+      id: 4,
+      path: '/technology',
+      text: '03 TECHNOLOGY'
+    },
+  ];
+  return(
+    <>
+      <header>
+        <nav>
+          <ul>
+            {links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path} exact>{link.text}</NavLink>
+            </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+};
+export default NavBar;
---
+++ src/component/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DesktopHeader() {
+return (
+  <h1>Testing</h1>
+)
+}
+
+export default DesktopHeader;
