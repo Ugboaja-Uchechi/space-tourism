2d62edeaba2cd4e13a49373d370d4ce6b1b135f6
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-03 11:55:03 +0100

    Create technology vehicle

---
+++ src/component/desktop/technologyvehicle.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function DesktopTechnology() {
+  return (
+    <>
+      <h3>03 Pick your Technology</h3>
+      <div>
+        <div>
+          <p>The terminology...</p>
+          <h2>Launch vehicle</h2>
+          <p>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+        </div>
+          <div></div>
+      </div>
+    </>
+  )
+};
+
+export default DesktopTechnology;
