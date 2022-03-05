e0cb37a1eca79d65a9acb0037aaca98241c99f29
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-05 23:03:32 +0100

    Adjust desktop home body style

--- src/component/desktop/index.js
+++ src/component/desktop/index.js
@@ -14,7 +14,6 @@ return (
           <p className='home-text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
         </div>
         <div className='explore-flex'>
-          {/* <div></div> */}
           <h4 className='explore'>Explore</h4>
         </div>
       </div>
