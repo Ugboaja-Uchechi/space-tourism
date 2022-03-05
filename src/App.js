c69103e46be2acc6182606f969462ae7c9808d99
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-05 15:27:45 +0100

    Add background image for desktop home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import NavBar from './component/Navbar';
 import DesktopHome from './component/desktop';
 import DesktopDestination from './component/desktop/destination';
 import DesktopCrew from './component/desktop/crewcommander';
+import DesktopTechnology from './component/desktop/technologyvehicle';
 
 function App() {
   return(
     <>
-      <NavBar />
       <Routes>
         <Route exact path='/' element={<DesktopHome />} />
         <Route exact path='/destination' element={<DesktopDestination />} />
         <Route exact path='/crew' element={<DesktopCrew />} />
+        <Route exact path='/technology' element={<DesktopTechnology />} />
       </Routes>
     </>
   )
--- src/component/desktop/index.js
+++ src/component/desktop/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import NavBar from '../Navbar';
+import './desktop.css';
 
 function DesktopHome() {
 return (
-  <>
+  <div className='background'>
+        <NavBar />
     <main>
       <div>
         <p>So, you want to travel to Space Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
@@ -11,7 +14,7 @@ return (
         </div>
       </div>
     </main>
-  </>
+  </div>
 )
 }
 
