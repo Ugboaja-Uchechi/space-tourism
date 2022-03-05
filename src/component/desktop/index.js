7b5cd65356d8e5da2761fe9a6d6a915147dcf423
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-05 22:27:51 +0100

    style desktop home body

--- src/component/desktop/index.js
+++ src/component/desktop/index.js
@@ -6,9 +6,13 @@ function DesktopHome() {
 return (
   <div className='background'>
         <NavBar />
-    <main>
-      <div>
-        <p>So, you want to travel to Space Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+    <main className='home-flex'>
+      <div className='bg-body'>
+        <div>
+          <p className='travel'>So, you want to travel to</p>
+          <p className='space'>Space </p>
+          <p className='home-text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        </div>
         <div>
           <h4>Explore</h4>
         </div>
