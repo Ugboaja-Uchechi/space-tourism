c0dc3786244fc92a1b0734493830445e1cd82f28
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-06 15:08:36 +0100

    Add flex to destination page

--- src/component/desktop/destination.js
+++ src/component/desktop/destination.js
@@ -1,17 +1,18 @@
 import React from "react";
 import NavBar from "../Navbar";
 import './destination.css';
+import moon from '../images/destination/image-moon.png'
 
 function DesktopDestination() {
   return (
-    <div className="home">
+    <div className="destination">
     <NavBar />
-      <h3>01 Pick your destination</h3>
-      <div>
+      <div className="destination-flex">
         <div>
-
+          <h3>01 Pick your destination</h3>
+          <img src={moon} alt="moon" className="moon" />
         </div>
-        <div>
+        <div className="destination-width">
           <h1>Moon</h1>
           <p>  See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
           <div></div>
