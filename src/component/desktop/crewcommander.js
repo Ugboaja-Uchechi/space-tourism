9ff9810857ee7daceb8473f0f03c09db9ec579da
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-12 22:58:52 +0100

    Add images

--- src/component/desktop/crewcommander.js
+++ src/component/desktop/crewcommander.js
@@ -1,10 +1,11 @@
 import React from "react";
 import NavBar from "../Navbar";
 import '../desktop/css/crew.css'
+import douglas from  '../images/crew/image-douglas-hurley.png'
 
 function DesktopCrew() {
   return (
-    <div>
+    <div className="crew">
       <NavBar />
       <h3>02 Pick your Crew</h3>
       <div>
@@ -13,7 +14,9 @@ function DesktopCrew() {
           <h2>Douglas Hurley</h2>
           <p>  Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
         </div>
-          <div></div>
+        <div>
+          <img src={douglas} alt="moon" className="douglas" />
+        </div>
       </div>
     </div>
   )
