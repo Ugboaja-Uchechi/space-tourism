452a68d2ad2caaf8024c44d1ee149dc3eb888425
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-13 14:14:44 +0100

    Align crew body

--- src/component/desktop/crewcommander.js
+++ src/component/desktop/crewcommander.js
@@ -7,9 +7,9 @@ function DesktopCrew() {
   return (
     <div className="crew">
       <NavBar />
-      <h3>02 Pick your Crew</h3>
-      <div>
+      <div className="crew-flex">
         <div>
+          <h3>02 Pick your Crew</h3>
           <h3>Commander</h3>
           <h2>Douglas Hurley</h2>
           <p>  Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
--- src/component/desktop/destination.js
+++ src/component/desktop/destination.js
@@ -14,7 +14,7 @@ function DesktopDestination() {
         </div>
         <div className="destination-width">
           <h1>Moon</h1>
-          <p className="text">  See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+          <p className="text">See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
           <div className="line"></div>
           <div className="distance">
             <div>
