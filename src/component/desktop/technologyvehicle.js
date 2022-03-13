672983566985985be7153e575a8580708c89f96f
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-13 15:35:35 +0100

    Style header nav

--- src/component/desktop/technologyvehicle.js
+++ src/component/desktop/technologyvehicle.js
@@ -1,8 +1,12 @@
 import React from "react";
+import NavBar from "../Navbar";
+import '../desktop/css/destination.css'
+import rocket from '../images/technology/image-launch-vehicle-portrait.jpg'
 
 function DesktopTechnology() {
   return (
-    <>
+    <div className="technology">
+      <NavBar />
       <h3>03 Pick your Technology</h3>
       <div>
         <div>
@@ -10,9 +14,11 @@ function DesktopTechnology() {
           <h2>Launch vehicle</h2>
           <p>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
         </div>
-          <div></div>
+        <div>
+        <img src={rocket} alt="rocket launch" className="rocket" />
+        </div>
       </div>
-    </>
+    </div>
   )
 };
 
