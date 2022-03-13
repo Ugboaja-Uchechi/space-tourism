90e8668dd91f835992d6724182bd7ae2b418227a
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-13 14:31:08 +0100

    Style texts

--- src/component/desktop/crewcommander.js
+++ src/component/desktop/crewcommander.js
@@ -7,12 +7,14 @@ function DesktopCrew() {
   return (
     <div className="crew">
       <NavBar />
+      <div>
+        <h3>02 Pick your Crew</h3>
+      </div>
       <div className="crew-flex">
         <div>
-          <h3>02 Pick your Crew</h3>
-          <h3>Commander</h3>
-          <h2>Douglas Hurley</h2>
-          <p>  Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+          <h3 className="commander">Commander</h3>
+          <h2 className="commander-name">Douglas Hurley</h2>
+          <p className="about-commander">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
         </div>
         <div>
           <img src={douglas} alt="moon" className="douglas" />
