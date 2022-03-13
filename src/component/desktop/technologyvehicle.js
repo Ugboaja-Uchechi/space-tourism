0bd0cd457ead4574f53944d87759cffab186d0ed
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-13 15:52:11 +0100

    Style launch vehicle text

--- src/component/desktop/technologyvehicle.js
+++ src/component/desktop/technologyvehicle.js
@@ -11,7 +11,7 @@ function DesktopTechnology() {
       <div className="technology-flex">
         <div>
           <p className="terminology">The terminology...</p>
-          <h2>Launch vehicle</h2>
+          <h2 className="launch-vehicle">Launch vehicle</h2>
           <p>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
         </div>
         <div>
