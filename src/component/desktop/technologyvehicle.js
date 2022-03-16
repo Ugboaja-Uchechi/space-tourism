027ed28a8fe343897d70fa1f07eb4515b41991e2
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-16 22:16:57 +0100

    Re-align content in tablet technology view page

--- src/component/desktop/technologyvehicle.js
+++ src/component/desktop/technologyvehicle.js
@@ -11,13 +11,16 @@ function DesktopTechnology() {
       <h3 className="space-launch">03 Space Launch 101</h3>
       <div className="technology-flex">
         <div>
+        <img src={rocketlandscape} alt="rocket launch" className="rocket-landscape" />
+        </div>
+        <div>
           <p className="terminology">The terminology...</p>
           <h2 className="launch-vehicle">Launch vehicle</h2>
           <p className="text">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
         </div>
         <div>
-            <img src={rocketlandscape} alt="rocket launch" className="rocket-landscape" />
-            <img src={rocket} alt="rocket launch" className="rocket" />
+          {/* <img src={rocketlandscape} alt="rocket launch" className="rocket-landscape" /> */}
+          <img src={rocket} alt="rocket launch" className="rocket" />
         </div>
       </div>
     </div>
