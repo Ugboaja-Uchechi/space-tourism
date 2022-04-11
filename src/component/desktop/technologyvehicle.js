a2c86e8b53d4ea3769a031355aea228852a0d714
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-04-11 20:31:17 +0100

    Create carousel for technology page

--- src/component/desktop/technologyvehicle.js
+++ src/component/desktop/technologyvehicle.js
@@ -9,7 +9,22 @@ function TechnologyCarousel() {
   return (
     <Carousel Fade>
       <Carousel.Item>
-        
+      <div className="d-flex align-items-center justify-content-evenly mobile-flex">
+        <div>
+          <img src={rocketlandscape} alt="rocket launch" className="rocket-landscape" />
+        </div>
+        <div>
+          <p className="terminology">The terminology...</p>
+          <h2 className="launch-vehicle">Launch vehicle</h2>
+          <p className="text">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+        </div>
+        <Carousel.Caption>
+        <div>
+          {/* <img src={rocketlandscape} alt="rocket launch" className="rocket-landscape" /> */}
+          <img src={rocket} alt="rocket launch" className="rocket" />
+        </div>
+        </Carousel.Caption>
+      </div>
       </Carousel.Item>
     </Carousel>
   )
@@ -23,7 +38,7 @@ function DesktopTechnology() {
         <h3 className="space-launch"><span className="numbers">03</span>Space Launch 101</h3>
       </div>
       <div className="technology-flex">
-        <technologyCarousel />
+        <TechnologyCarousel />
       </div>
     </div>
   )
